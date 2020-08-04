4c051f4282ef8ce08e9c052f1043302598b66143
devinchambers <69162578+devinchambers@users.noreply.github.com>
2020-08-04 01:09:35 -0600

    Add files via upload

---
+++ sickos/sketch.js
@@ -0,0 +1,449 @@
+var song;
+var song2;
+var song3;
+var song4;
+var song5;
+var song6;
+var song7;
+var song8;
+var song9;
+var song10;
+var song11;
+var song12;
+var song13;
+var song14;
+var song15;
+var song16;
+var song17;
+var song18;
+var song19;
+var song20;
+var song21;
+var song22;
+var song23;
+var song24;
+var song25;
+var song26;
+var song27;
+var song28;
+var song29;
+var song30;
+var song31;
+var song32;
+var song33;
+var song34;
+var song35;
+var song36;
+var song37;
+var song38;
+var song39;
+var song40;
+var song41;
+var song42;
+var song43;
+var song44;
+var song45;
+
+
+
+function preload() {
+  song = loadSound ("1.wav");
+  song2 = loadSound ("2.wav");
+  song3 = loadSound ("3.wav");
+  song4 = loadSound ("4.wav");
+  song5 = loadSound ("5.wav");
+   song6 = loadSound ("6.wav");
+  song7 = loadSound ("7.wav");
+  song8 = loadSound ("8.wav");
+  song9 = loadSound ("9.wav");
+  song10 = loadSound ("10.wav");
+   song11 = loadSound ("11.wav");
+  song12 = loadSound ("12.wav");
+  song13 = loadSound ("13.wav");
+  song14 = loadSound ("14.wav");
+  song15 = loadSound ("15.wav");
+   song16 = loadSound ("16.wav");
+  song17 = loadSound ("17.wav");
+  song18 = loadSound ("18.wav");
+  song19 = loadSound ("19.wav");
+  song20 = loadSound ("20.wav");
+   song21 = loadSound ("21.wav");
+  song22 = loadSound ("22.wav");
+  song23 = loadSound ("23.wav");
+  song24 = loadSound ("24.wav");
+  song25 = loadSound ("25.wav");
+   song26 = loadSound ("26.wav");
+  song27 = loadSound ("27.wav");
+  song28 = loadSound ("28.wav");
+  song29 = loadSound ("29.wav");
+  song30 = loadSound ("30.wav");
+ song31 = loadSound ("31.wav");
+  song32 = loadSound ("32.wav");
+  song33 = loadSound ("33.wav");
+  song34 = loadSound ("34.wav");
+  song35 = loadSound ("35.wav");
+   song36 = loadSound ("36.wav");
+  song37 = loadSound ("37.wav");
+  song38 = loadSound ("38.wav");
+  song39 = loadSound ("39.wav");
+  song40 = loadSound ("40.wav");
+   song41 = loadSound ("41.wav");
+  song42 = loadSound ("42.wav");
+  song43 = loadSound ("43.wav");
+  song44 = loadSound ("44.wav");
+  song45 = loadSound ("45.wav");
+  
+
+
+}
+function setup() {
+  createCanvas(1860, 1000);
+  background(255);
+ textSize(20);
+text('press the correct key', 600, 500);
+}
+
+function keyPressed() {
+  
+ if (key == 'q') {
+   background(255);
+      textSize(30);
+     text('I have great hair.', 200, 300);
+   song.play();
+   
+   }
+
+if (key == 'w') {
+   background(255);
+      textSize(30);
+     text('When it is watched and cut.', 750, 750);
+   song2.play();
+   
+   }
+    
+   if (key == 'e') {
+   background(255);
+      textSize(30);
+     text('Am I balding?', 200, 300);
+   song3.play();
+   
+   }
+
+if (key == 'r') {
+   background(255);
+      textSize(30);
+     text('When it is watched and cut.', 750, 750);
+   song4.play();
+   
+   }
+   if (key == 't') {
+   background(255);
+      textSize(30);
+     text('How are you ageing?', 200, 300);
+   song5.play();
+   
+   }
+
+if (key == 'y') {
+   background(255);
+      textSize(30);
+     text('maybe I am gaining weight?', 750, 750);
+   song6.play();
+   
+   }
+   if (key == 'u') {
+   background(255);
+      textSize(30);
+     text('Your face is fuller',200, 300);
+   song7.play();
+   
+   }
+
+if (key == 'i') {
+   background(255);
+      textSize(30);
+     text('Does it look tired? is my skin more worn?', 750, 750);
+   song8.play();
+   
+   }
+   if (key == 'o') {
+   background(255);
+      textSize(30);
+     text('I suppose so but I like how your skin is.', 200, 300);
+   song9.play();
+   
+   }
+
+if (key == 'p') {
+   background(255);
+      textSize(30);
+     text('Even the scars?', 750, 750);
+   song10.play();
+   
+   }
+  if (key == 's') {
+   background(255);
+      textSize(30);
+     text('I remeber walking through a drying a lakebed and sitting in the moonlight   ',200, 300);
+   song45.play();
+   
+   }
+   if (key == 'a') {
+   background(255);
+      textSize(30);
+     text('Even the scars and the dry wrinkle on your elbow', 200, 300);
+   song11.play();
+   
+   }
+
+if (key == 'd') {
+   background(255);
+      textSize(30);
+     text('My elbow is probably where I loose the most skin', 750, 750);
+   song12.play();
+   
+   }
+   if (key == 'f') {
+   background(255);
+      textSize(30);
+     text('My feet are probably the most worn part of my body', 200, 300);
+   song13.play();
+   
+   }
+
+if (key == 'g') {
+   background(255);
+      textSize(30);
+     text('How are your toenails?', 750, 750);
+   song14.play();
+   
+   }
+   if (key == 'h') {
+   background(255);
+      textSize(30);
+     text('They are becoming discolored and thick', 200, 300);
+   song15.play();
+   
+   }
+
+if (key == 'j') {
+   background(255);
+      textSize(30);
+     text('Mine are still sharp',750, 750);
+   song16.play();
+   
+   }
+   if (key == 'k') {
+   background(255);
+      textSize(30);
+     text('Remeber when we were in the water and you scratched my legs', 200, 300);
+   song17.play();
+   
+   }
+
+if (key == 'l') {
+   background(255);
+      textSize(30);
+     text('Yeah, I also remeber when you passed the ball to the wrong person', 750, 750);
+   song18.play();
+   
+   }
+   if (key == 'z') {
+   background(255);
+      textSize(30);
+     text('Or when we tried to sit on the balls and they shot out from underneath us', 200, 300);
+   song19.play();
+   
+   }
+
+if (key == 'x') {
+   background(255);
+      textSize(30);
+     text('I remeber walking home in the cold with wet hair and itchy eyes', 750, 750);
+   song20.play();
+   
+   }
+   if (key == 'c') {
+   background(255);
+      textSize(30);
+     text('Sometimes I would piss in the hot tub', 200, 300);
+   song21.play();
+   
+   }
+
+if (key == 'v') {
+   background(255);
+      textSize(30);
+     text('Is that somthing everyone does and noone admits to, or are we just sickos? ', 750, 750);
+   song22.play();
+   
+   }
+   if (key == 'b') {
+   background(255);
+      textSize(30);
+     text('I am not sure if we are unique enough to be sickos', 200, 300);
+   song23.play();
+   
+   }
+
+if (key == 'n') {
+   background(255);
+      textSize(30);
+     text('I think it is that we are not sick enough to be sickos', 750, 750);
+   song24.play();
+   
+   }
+   if (key == 'm') {
+   background(255);
+      textSize(30);
+     text('We are just an average level of perversion', 200, 300);
+   song25.play();
+   
+   }
+
+if (key == ',') {
+   background(255);
+      textSize(30);
+     text('Maybe some day we can be full blown perverts', 750, 750);
+   song26.play();
+   
+   }
+   if (key == '.') {
+   background(255);
+      textSize(30);
+     text('One time I hit my tow with a crow bar and under the nail filled with blood until the nail came off ', 200, 300);
+   song27.play();
+   
+   }
+
+if (key == '/') {
+   background(255);
+      textSize(30);
+     text('It must have been satifying to have a cleanly cut toe', 750, 750);
+   song28.play();
+   
+   }
+   if (key == '1') {
+   background(255);
+      textSize(30);
+     text('Do you think if we would have gone to the gravel pit we would have drown?', 200, 300);
+   song29.play();
+   
+   }
+
+if (key == '2') {
+   background(255);
+      textSize(30);
+     text('my mom had an electric bread maker', 750, 750);
+   song30.play();
+   
+   }
+   if (key == '3') {
+   background(255);
+      textSize(30);
+     text('what kind of bread did she bake?', 200, 300);
+   song31.play();
+   
+   }
+
+if (key == '4') {
+   background(255);
+      textSize(30);
+     text('She didnt bake bread, it stayed in the box until we gave it away', 750, 750);
+   song32.play();
+   
+   }
+   if (key == '5') {
+   background(255);
+      textSize(30);
+     text('my mom made banana and it was really warm and soft ', 200, 300);
+   song33.play();
+   
+   }
+
+if (key == '6') {
+   background(255);
+      textSize(30);
+     text('my we played with crayons if she could sit me down, I cant remeber', 750, 750);
+   song34.play();
+   
+   }
+   if (key == '7') {
+   background(255);
+      textSize(30);
+     text('I remeber the taste of crayons, they are not bad but they make your mouth dry', 200, 300);
+   song35.play();
+   
+   }
+
+if (key == '8') {
+   background(255);
+      textSize(30);
+     text('most of the time when I wake up my mouth is dry', 750, 750);
+   song36.play();
+   
+   }
+   if (key == '9') {
+   background(255);
+      textSize(30);
+     text('I dreamed I had a black spoon and I was in a sauna with friends, but I couldnt look at them  ', 200, 300);
+   song37.play();
+   
+   }
+
+if (key == '0') {
+   background(255);
+      textSize(30);
+     text('Then were you bit by two snakes, one big and one small', 750, 750);
+   song38.play();
+   
+   }
+   if (key == '-') {
+   background(255);
+      textSize(30);
+     text('No, then she was there but she wouldnt look at me, she just sat in the corner', 200, 300);
+   song39.play();
+   
+   }
+
+if (key == '=') {
+   background(255);
+      textSize(30);
+     text('Maybe you really are a pervert', 750, 750);
+   song40.play();
+   
+   }
+   if (key == '[') {
+   background(255);
+      textSize(30);
+     text('Yeah well, you area hippy talking about dreams', 200, 300);
+   song41.play();
+   
+   }
+
+if (key == ']') {
+   background(255);
+      textSize(30);
+     text('Are you going away with me?', 750, 750);
+   song42.play();
+   
+   }
+   if (key == ';') {
+   background(255);
+      textSize(30);
+     text('The land is falling into the river creating great pools of muck', 200, 300);
+   song43.play();
+   
+   }
+
+if (key == '`') {
+   background(255);
+      textSize(30);
+     text('we found a dead deer and buried it like good kids', 750, 750);
+   song44.play();
+   
+   }
+
+ textSize(20);
+text('press the correct key', 600, 500);
+}
